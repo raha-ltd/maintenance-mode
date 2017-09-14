b7878add98677c8dcf0fe0c6985b3acd9b5ca6e5
siavash <siavash.kavousi.sk@gmail.com>
2017-09-14 16:37:57 +0430

    feat: add termination plugin studio cmd

--- run.sh
+++ run.sh
@@ -4,7 +4,13 @@ index=$(cat ./dist/index.html)
 
 curl -X POST http://localhost:8001/apis/lms/plugins \
     --data "name=request-termination" \
-    --data "config.status_code=403" \
+    --data "config.status_code=503" \
+    --data "config.body=$index" \
+    --data "config.content_type=text/html charset=utf-8;"
+
+curl -X POST http://localhost:8001/apis/studio/plugins \
+    --data "name=request-termination" \
+    --data "config.status_code=503" \
     --data "config.body=$index" \
     --data "config.content_type=text/html charset=utf-8;"
 
--- src/App.vue
+++ src/App.vue
@@ -53,5 +53,3 @@
     span
       font-size 2rem
 </style>
-
-<style></style>
