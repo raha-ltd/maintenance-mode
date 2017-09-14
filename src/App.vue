6267ae9e13d30e2e847863bf4db9868930536ae8
siavash <siavash.kavousi.sk@gmail.com>
2017-09-14 16:16:07 +0430

    style: app variables like in lms

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,6 @@
     view="lHh Lpr fff"
   >
     <q-toolbar slot="header">
-      <!--<q-btn-->
-      <!--flat-->
-      <!--@click="$refs.layout.toggleLeft()"-->
-      <!--&gt;-->
-      <!--<q-icon name="menu" />-->
-      <!--</q-btn>-->
-
       <q-toolbar-title>
         Raha Platform
         <div slot="subtitle">Under Maintenance</div>
@@ -18,7 +11,7 @@
     </q-toolbar>
     <div class="layout-padding row justify-center items-center" style="min-height: inherit;">
       <div class="main column items-center">
-        <span>Our platform is under maintenance</span>
+        <span>Site is under maintenance</span>
         <span>please come back later</span>
       </div>
     </div>
