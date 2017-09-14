65d948185a631e4d9cc65b35f038dbc32660b7cb
siavash <siavash.kavousi.sk@gmail.com>
2017-09-14 12:48:36 +0430

    chore: add run script

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+index=`cat ./dist/index.html`
+
+curl -X POST http://localhost:8001/apis/lms/plugins \
+    --data "name=request-termination" \
+    --data "config.status_code=403" \
+    --data "config.body=$index" \
+    --data "config.content_type=text/html charset=utf-8;"
+
